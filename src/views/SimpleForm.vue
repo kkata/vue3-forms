be588f2417af08506fefc3b1e4fe3291b3d0c97e
kkata <14849535+kkata@users.noreply.github.com>
2022-10-31 20:33:25 +0900

    9. basic a11y for our components

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -8,38 +8,56 @@
         label="Select a category"
       />
 
-      <h3>Name & describe your event</h3>
+      <fieldset>
+        <legend>Name & describe your event</legend>
 
-      <BaseInput v-model="event.title" label="Title" type="text" />
-
-      <BaseInput v-model="event.description" label="Desctiption" type="text" />
-
-      <h3>Where is your event?</h3>
-
-      <BaseInput v-model="event.location" label="Location" type="text" />
-
-      <h3>Are pets allowed?</h3>
-      <div>
-        <BaseRadioGroup
-          v-model="event.pets"
-          name="pets"
-          :options="petOptions"
+        <BaseInput
+          v-model="event.title"
+          label="Title"
+          type="text"
+          error="This input has an error!"
         />
-      </div>
 
-      <h3>Extras</h3>
-      <div>
-        <BaseCheckBox v-model="event.extras.catering" label="Catering" />
-      </div>
+        <BaseInput
+          v-model="event.description"
+          label="Description"
+          type="text"
+        />
+      </fieldset>
 
-      <div>
-        <BaseCheckBox v-model="event.extras.music" label="Live music" />
-      </div>
+      <fieldset>
+        <legend>Where is your event?</legend>
 
-      <pre>{{ JSON.stringify(event, null, 2) }}</pre>
+        <BaseInput v-model="event.location" label="Location" type="text" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Pets</legend>
+        <p>Are pets allowed?</p>
+        <div>
+          <BaseRadioGroup
+            v-model="event.pets"
+            name="pets"
+            :options="petOptions"
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Extras</legend>
+        <div>
+          <BaseCheckBox v-model="event.extras.catering" label="Catering" />
+        </div>
+
+        <div>
+          <BaseCheckBox v-model="event.extras.music" label="Live music" />
+        </div>
+      </fieldset>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
     </form>
+
+    <pre>{{ JSON.stringify(event, null, 2) }}</pre>
   </div>
 </template>
 
@@ -92,3 +110,17 @@ export default {
   }
 };
 </script>
+
+<style>
+fieldset {
+  border: 0;
+  margin: 0;
+  padding: 0;
+}
+
+legend {
+  font-size: 28px;
+  font-weight: 700;
+  margin-top: 20px;
+}
+</style>
